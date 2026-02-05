4f6c01c7893768f1f5c18c353227ef46fd715fd1
Louis <louis.mueller@mujas.com>
2026-02-05 10:00:46 +0100

    configurating the models.py in tracker and changing the settings.py in config

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Company(models.Model):
+        #Base-Info
+        name = models.CharField(max_length=200)
+        website = models.URLField(blank=True, null=True)
+
+        #API-Data
+        industry = models.CharField(max_length=200, blank=True, null=True)
+        description = models.TextField(blank=True, null=True)
+        employee_count = models.CharField(max_length=100, blank=True, null=True)
+        revenue = models.CharField(max_length=100, blank=True, null=True)
+        country = models.CharField(max_length=100, blank=True, null=True)
+        logo_url= models.URLField(blank=True, null=True)
+
+        def __str__(self):
+                return str.name
+
+class Application(models.Model):
+        #Status Choices for the dropdown menu
+        STATUS_CHOICES = [
+                ('saved', 'Saved'),
+                ('applied', 'Applied'),
+                ('interview', 'Interview'),
+                ('offer', 'Offer'),
+                ('rejected', 'Rejected'),
+        ]
+
+        company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='applied')
+
+        status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='saved')
+        role = models.CharField(max_length=200, help_text='e.g. Software Engineer Intern')
+        job_link = models.URLField(blank=True, null=True)
+        date_applied = models.DateField(default=timezone.now)
+        notes = models.TextField(blank=True, null=True)
+
+        def __str__(self):
+                return f"{self.role} at {self.company.name}"
