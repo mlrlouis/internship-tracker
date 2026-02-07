57a595f611b2a8dd2d73dc4c30232fba1667de85
Louis <louis.mueller@mujas.com>
2026-02-07 11:05:07 +0100

    replace CompanyForm with ApplicationForm and update view logic for applications

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,14 +1,41 @@
 from django import forms
+from models import Application
 
-
-class CompanyForm(forms.Form):
-        symbol = forms.CharField(
-                label="Ticker Symbol",
-                max_length=50,
-                help_text="e. g. AAPL for Apple or MSFT for Microsoft",
+class ApplicationForm(forms.Form):
+        company_name = forms.CharField(
+                label="Company Name",
+                max_length=200,
                 widget=forms.TextInput(attrs={
-                        'placeholder': 'AAPL',
+                        'placeholder': 'e.g. Microsoft, Apple, BMW or local bakery',
                         'class': 'input-field',
                         'autofocus': 'autofocus'
                 })
         )
+
+        company_symbol = forms.CharField(
+                label="Optional Ticker Symbol",
+                required=False,
+                help_text="Only needed if you want to fetch stock data (e.g. AAPL)",
+                widget=forms.TextInput(attrs={
+                        'placeholder': 'e.g. AAPL or MSFT',
+                        'class': 'input-filed'
+                })
+        )
+
+        class Meta:
+                model = Application
+                fields = ['role', 'status', 'job_link', 'notes']
+                widgets = {
+                        'role': forms.TextInput(attrs={
+                                'placeholder': 'e.g. Software Engineer intern'
+                        }),
+
+                        'job_link': forms.URLInput(attrs={
+                                'placeholder': 'https://...'
+                        }),
+
+                        'notes': forms.Textarea(attrs={
+                                'rows': 5,
+                                'placeholder': 'Notes: Salary, Interview feedback, etc.'
+                        })
+                }
--- tracker/views.py
+++ tracker/views.py
@@ -1,40 +1,54 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import CompanyForm
-from .models import Company
-from .services.company_api import save_company_to_db
+from .forms import ApplicationForm
+from .models import Company, Application
+from .services.company_api import get_company_profile
 
 # Create your views here.
 def company_list(request):
-        # Case 1: User clicked 'Add Company'
+        # Case 1: Adding new application
         if request.method == 'POST':
-                form = CompanyForm(request.POST)
+                form = ApplicationForm(request.POST)
 
                 if form.is_valid():
                         # Get the clean data from the form
-                        symbol = form.cleaned_data['symbol']
+                        company_name = form.cleaned_data['company_name']
+                        symbol = form.cleaned_data['company_symbol']
 
-                        # Call the service to fetch data from Yahoo and save to DB
-                        new_company = save_company_to_db(symbol)
-
-                        # Give feedback to the user
-                        if new_company:
-                                messages.success(request, f"{new_company.name} successfully added!")
-                        else:
-                                messages.error(request, f"Could not find data for {symbol}.")
+                        # Check if company already exists
+                        company, created = Company.get_or_create(name=company_name)
                         
-                        # Reloading the page to clear the form
+                        # If company is new and as a symbol exists -> get Yahoo data
+                        if created and symbol:
+                                api_data = get_company_profile(symbol)
+                                if api_data:
+                                        # Writing the Yahoo data into Company
+                                        company.industry = api_data.get('industry')
+                                        company.description = api_data.get('description')
+                                        company.employee_count = api_data.get('employee_count')
+                                        company.revenue = api_data.get('revenue')
+                                        company.website = api_data.get('website')
+                                        company.save()
+                                        messages.info(request, f"Fetched data for {company_name} from Yahoo!")
+                        elif created:
+                                messages.info(request, f"Created manual entry for {company_name}")
+                        
+                        # saving the application
+                        application = form.save(commit=False)
+                        application.company = company
+                        application.save()
+
+                        messages.success(request, "Application added successfully!")
                         return redirect('company_list')
-        # Case 2: User just opened the page
         else:
-                form = CompanyForm()
+                form = ApplicationForm()
         
-        # Always fetch all the companies to show them in the list
-        companies = Company.objects.all().order_by('-id')
+        # Case 2: Show list
+        applications = Application.objects.all().order_by('-date_applied')
 
         context = {
                 'form': form,
-                'companies': companies
+                'applications': applications
         }
 
         return render(request, 'tracker/company_list.html', context)
